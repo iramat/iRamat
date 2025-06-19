679174e42c1a337d5e3961cbfd73627857aa64c7
HUET Thomas <TH282424@extra.cea.fr>
2025-06-19 09:38:32 +0200

    varia

--- R/ppa.R
+++ R/ppa.R
@@ -5,64 +5,79 @@
 #'
 #' @param root The path to the raster dataset.
 #' @param img.paths Name of the images.
-#' @param ppa_tests A list of test to perform, in a vector. By default: Quadrat Test ("quadrat"), Ripley’s K ("ripley"), G-function ("gfunction"). The vector looks like: c("quadrat", "ripley", "gfunction")
+#' @param ppa_tests A list of test to perform, in a vector. By default: Quadrat Test ("quadrat"), Ripley’s K ("ripley"), G-function ("gfunction"). The vector looks like: c("quadrat", "ripley", "gfunction"). R quadrat.test() function: If p < 0.05 → the pattern is likely non-random (either clustered or regular). R Kest() (Ripley) function: ... R Gest() (G-function) function: ...
 #' @param verbose if TRUE (by default), verbose.
 #'
 #' @return Geostatistical test results.
 #'
 #' @examples
-#'
-#' Kfar_Hahoresh.crane_afg_nd_pl_polygon.wkt <- conv_geojson_to_wkt()
-#'
-#'
+#' d <- ppa()
+#' # Show results of Quadrat test of the clustered distribution
+#' d$clustered_distribution.png$quadrat
+#' # Plot the K-Ripley of the clustered distribution
+#' plot(d$clustered_distribution.png$ripley, main = "clustered distribution")
+#' # Plot the G-function of the regular distribution
+#' plot(d$regular_distribution.png$gfunction, main = "regular distribution")
 
-# Install if necessary
-# install.packages("imager")
-
-# library(imager)
-# library(spatstat.geom)
-# library(spatstat)
-
-# Load image
-
-ppa <- function(root = "https://raw.githubusercontent.com/iramat/iRamat/master/inst/extdata/",
+ppa <- function(d = NA,
+                root = "https://raw.githubusercontent.com/iramat/iRamat/master/inst/extdata/",
                 img.paths = c("clustered_distribution.png", 
                               "random_distribution.png", 
                               "regular_distribution.png"),
                 ppa_tests = c("quadrat", "ripley", "gfunction"),
                 verbose = TRUE){
+  if (is.na(d)){
+    if(verbose){
+      print(paste0("Will store the results in a new variable"))
+    }
+    d <- hash::hash()
+  }
   for (i in seq(1, length(img.paths))){
-    # i <- 3
-    print(paste0("Read: ", img.paths[i]))
-    img.path <- paste0(root, img.paths[i])
-    img <- load.image(img.path)
+    # i <- 1
+    im <- img.paths[i]
+    if(verbose){
+      print(paste0("Read image: ", im))
+    }
+    img.path <- paste0(root, im)
+    img <- imager::load.image(img.path)
     # Convert to grayscale if needed
-    img_gray <- grayscale(img)
+    img_gray <- imager::grayscale(img)
     # Threshold to binary (assuming black points on white background)
     img_bin <- img_gray < 0.5
     # Extract black pixel coordinates
     coords <- which(img_bin, arr.ind = TRUE)
     points_df <- data.frame(x = coords[, 2], y = coords[, 1])  # Swap due to matrix order
-    # Define window (image is 100x100)
-    window <- owin(xrange = c(0, 100), yrange = c(0, 100))
+    # window <- owin(xrange = c(0, 100), yrange = c(0, 100)) # Define window (image is 100x100)
+    window <- spatstat.geom::owin(xrange = c(0, ncol(img)), 
+                                  yrange = c(0, nrow(img)))
     # Create point pattern object
-    pp <- ppp(points_df$x, points_df$y, window = window)
+    pp <- spatstat.geom::ppp(points_df$x, points_df$y, window = window)
     
     # Tests
-    quadrat.test(pp, nx = 5, ny = 5) # If p < 0.05 → the pattern is likely non-random (either clustered or regular).
-    #
-    K <- Kest(pp)
-    plot(K)
-    #
-    G <- Gest(pp)
-    plot(G)
+    #   | Method       | Random      | Clustered | Regular   |
+    #   | ------------ | ----------- | --------- | --------- |
+    #   | Quadrat Test | p > 0.05    | p < 0.05  | p < 0.05  |
+    #   | Ripley’s K   | Matches CSR | Above CSR | Below CSR |
+    #   | G-function   | Matches CSR | Steeper   | Flatter   |
+    
+    if("quadrat" %in% ppa_tests){
+      if(verbose){
+        print(paste0("Runs Quadrat test"))
+      }
+      d[[im]][["quadrat"]] <- spatstat.explore::quadrat.test(pp, nx = 5, ny = 5)
+    }
+    if("ripley" %in% ppa_tests){
+      if(verbose){
+        print(paste0("Runs K-Ripley test"))
+      }
+      d[[im]][["ripley"]] <- spatstat.explore::Kest(pp)
+    }
+    if("gfunction" %in% ppa_tests){
+      if(verbose){
+        print(paste0("Runs K-Ripley test"))
+      }
+      d[[im]][["gfunction"]] <- spatstat.explore::Gest(pp)
+    }
   }
+  return(d)
 }
-
-#   | Method       | Random      | Clustered | Regular   |
-#   | ------------ | ----------- | --------- | --------- |
-#   | Quadrat Test | p > 0.05    | p < 0.05  | p < 0.05  |
-#   | Ripley’s K   | Matches CSR | Above CSR | Below CSR |
-#   | G-function   | Matches CSR | Steeper   | Flatter   |
-
-
