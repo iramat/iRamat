ab227ab8a791040dbe9d4170597465e6291d00f4
HUET Thomas <TH282424@extra.cea.fr>
2025-09-16 10:43:58 +0200

    varia

--- R/chrono.R
+++ R/chrono.R
@@ -3,72 +3,67 @@
 #' @name chrono
 #' @description Read EDTF data and return a ggplot timeline
 #'
-#' @param d A hash object. If none is provided, a new one will be created.
-#' @param api_url An URL landing to an API. Default: 'dataset_adisser17'.
-#' @param output_format The selected output format. Default "dataframe".
-#' @param verbose if TRUE (by default), verbose.
+#' @param d A dataframe 
+#' @param seriated if TRUE (default), re-order by minimum (earliest) date.
+#' @param verbose if TRUE (default), verbose.
 #'
-#' @return An R object.
+#' @return A ggplot graph.
 #'
 #' @examples
 #' df <- db_api_connect()
-#' # Show the dataset timeline
+#' # Show the dataset timeline (seriated)
 #' chrono(df$dataset_adisser17)
+#' # Show the dataset timeline (not seriated)
+#' chrono(df$dataset_adisser17, seriated = FALSE)
 #' 
 #' @export
 chrono <- function(d = NA,
+                   seriated = TRUE,
                    verbose = TRUE){
-  library(dplyr)
-  library(stringr)
-  library(ggplot2)
-  library(forcats)
-  
-  df_parsed <- df$dataset_adisser17 %>%
-    filter(!is.na(edtf)) %>%
-    mutate(
-      start_str = str_extract(edtf, "^[^/]+"),
-      end_str   = str_extract(edtf, "(?<=/).+$"),
-      
-      start_num = as.integer(str_extract(start_str, "-?\\d+")),
-      end_num   = as.integer(str_extract(end_str, "-?\\d+")),
-      
-      start_uncertain = str_detect(start_str, "\\?"),
-      end_uncertain   = str_detect(end_str, "\\?"),
-      start_approx    = str_detect(start_str, "~"),
-      end_approx      = str_detect(end_str, "~")
+  `%>%` <- dplyr::`%>%` 
+  df_parsed <- d %>%
+    dplyr::filter(!is.na(edtf)) %>%
+    dplyr::mutate(
+      start_str = stringr::str_extract(edtf, "^[^/]+"),
+      end_str   = stringr::str_extract(edtf, "(?<=/).+$"),
+      start_num = as.integer(stringr::str_extract(start_str, "-?\\d+")),
+      end_num   = as.integer(stringr::str_extract(end_str, "-?\\d+")),
+      start_uncertain = stringr::str_detect(start_str, "\\?"),
+      end_uncertain   = stringr::str_detect(end_str, "\\?"),
+      start_approx    = stringr::str_detect(start_str, "~"),
+      end_approx      = stringr::str_detect(end_str, "~")
     ) %>%
-    # re-order site_name by minimum (earliest) date
-    mutate(site_name = fct_reorder(site_name, start_num, .fun = min, .desc = TRUE))
-  
-  gg <- ggplot(df_parsed) +
-    geom_segment(aes(
+    { if (seriated) 
+      dplyr::mutate(., site_name = forcats::fct_reorder(site_name, start_num, .fun = min, .desc = TRUE))
+      else . }
+  site_legend <- ifelse(seriated, "Sites (ordered by earliest date)", "Sites")
+  gg <- ggplot2::ggplot(df_parsed) +
+    ggplot2::geom_segment(ggplot2::aes(
       x = start_num, xend = end_num,
       y = site_name, yend = site_name
     ), size = 1) +
+    ggplot2::geom_point(ggplot2::aes(x = start_num, y = site_name),
+                        data = dplyr::filter(df_parsed, start_uncertain),
+                        shape = 4, size = 3, color = "black") +
+    ggplot2::geom_point(ggplot2::aes(x = end_num, y = site_name),
+                        data = dplyr::filter(df_parsed, end_uncertain),
+                        shape = 4, size = 3, color = "black") +
+    ggplot2::geom_point(ggplot2::aes(x = start_num, y = site_name),
+                        data = dplyr::filter(df_parsed, start_approx),
+                        shape = 3, size = 3, color = "black") +
+    ggplot2::geom_point(ggplot2::aes(x = end_num, y = site_name),
+                        data = dplyr::filter(df_parsed, end_approx),
+                        shape = 3, size = 3, color = "black") +
     
-    geom_point(aes(x = start_num, y = site_name),
-               data = filter(df_parsed, start_uncertain),
-               shape = 4, size = 3, color = "black") +
-    geom_point(aes(x = end_num, y = site_name),
-               data = filter(df_parsed, end_uncertain),
-               shape = 4, size = 3, color = "black") +
-    
-    geom_point(aes(x = start_num, y = site_name),
-               data = filter(df_parsed, start_approx),
-               shape = 3, size = 3, color = "black") +
-    geom_point(aes(x = end_num, y = site_name),
-               data = filter(df_parsed, end_approx),
-               shape = 3, size = 3, color = "black") +
-    
-    theme_minimal() +
-    labs(
-      x = "Year",
-      y = "Site (ordered by earliest date)",
+    ggplot2::theme_minimal() +
+    ggplot2::labs(
+      x = "BCE/CE",
+      y = site_legend,
       title = "Timeline of sites with EDTF intervals",
       subtitle = "Cross = uncertain (?), Plus = approximate (~)"
     )
   return(gg)
 }
 
-df <- db_api_connect()
-chrono(df$dataset_adisser17)
+# df <- db_api_connect()
+# chrono(d = df$dataset_adisser17, seriated = TRUE)
