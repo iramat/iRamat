a17aee5011e0ec8732f61cdcb8e0c9fe0af3a4d9
HUET Thomas <TH282424@extra.cea.fr>
2026-05-21 10:30:57 +0200

    cleaned files created during the local check

--- iRamat.Rcheck/00_pkg_src/iRamat/tests/testthat/test-db_apiconnect.R
+++
@@ -1,5 +0,0 @@
-test_that("db_api_connect returns a dataframe", {
-  # skip("Needs live DB connection") # optional, if you can’t connect in CI
-  df <- db_api_connect()
-  expect_s4_class(df, "hash")
-})
